8747b8ec44a2f4ccd507cdcb188f5af3b77883b8
Juanes6767 <159493494+Juanes6767@users.noreply.github.com>
2024-02-19 17:08:32 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+class Motor:
+    def __init__(self,numeroCilindros, tipo, registro):
+        self.numeroCilindros=numeroCilindros
+        self.tipo=tipo
+        self.registro=registro
+    def cambiarRegistro(self, registro):
+        if registro!=None:
+            self.registro=registro
+    def asignarTipo(self,tipo):
+        if tipo=="electrico" or tipo=="gasolina":
+            self.tipo=tipo
 class Auto:
     cantidadCreados=0
     def __init__(self,modelo,precio,asientos, marca, motor,registro):
@@ -33,14 +44,3 @@ class Asiento:
     def cambiarColor(self,color):
         if(color=="rojo" or color=="verde" or color=="amarillo" or color=="negro" or color=="blanco"):
             self.color=color
-class Motor:
-    def __init__(self,numeroCilindros, tipo, registro):
-        self.numeroCilindros=numeroCilindros
-        self.tipo=tipo
-        self.registro=registro
-    def cambiarRegistro(self, registro):
-        if registro!=None:
-            self.registro=registro
-    def asignarTipo(self,tipo):
-        if tipo=="electrico" or tipo=="gasolina":
-            self.tipo=tipo
